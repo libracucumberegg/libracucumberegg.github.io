246079a6069fd08fa9b325c47b90ece3a7f6434d
libracucumberegg <markus.hansson04@outlook.com>
2023-07-06 14:19:20 -0700

    add page

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+let button = document.getElementById("scroll");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
